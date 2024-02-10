2295625183611da44ebc1a30b2fb47dea4e403f7
francisohara24 <franciskohara@gmail.com>
2024-02-09 22:31:31 -0500

    Feat: remove redundant Main source file.

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
-
-// command for checking version of Java installed
-// java --version
