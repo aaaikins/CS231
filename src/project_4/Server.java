39c518a6a296e81fdf7fa41694a9794840811dfd
francisohara24 <franciskohara@gmail.com>
2024-03-24 20:09:32 -0400

    Style: add whitespace.

--- src/project_4/Server.java
+++ src/project_4/Server.java
@@ -8,6 +8,7 @@ How to Run:  N/A
 package project_4;
 
 import project_4.lab_4.LinkedList;
+
 import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Toolkit;
@@ -47,7 +48,7 @@ public class Server {
     /**
      * Creates a new Server object.
      */
-    public Server(){
+    public Server() {
         queue = new LinkedList<>();
         time = 0.0;
         totalWaitingTime = 0.0;
@@ -58,17 +59,19 @@ public class Server {
 
     /**
      * Adds the specified job `job` into the queue.
+     *
      * @param job The job to be added to the queue.
      */
-    public void addJob(Job job){
+    public void addJob(Job job) {
         // todo: Don't we need to update the arrivalTime field of the job with the server's `time` field?
         queue.offer(job);
         remainingTime += job.getProcessingTimeNeeded();
-        size ++;
+        size++;
     }
 
     /**
      * Processes jobs currently in the queue up to the time specified by `time` and updates the server's system time accordingly.
+     *
      * @param time The time at which the jobs currently in the queue should be processed till.
      */
     public void processTo(double time) {
@@ -77,18 +80,18 @@ public class Server {
         // process the job(s)
         double timeLeft = time - this.time;
 
-        while (timeLeft > 0){
+        while (timeLeft > 0) {
             Job currentJob = queue.peek();
-            double timeToProcessFor =  Math.min(timeLeft, currentJob.getProcessingTimeRemaining());
+            double timeToProcessFor = Math.min(timeLeft, currentJob.getProcessingTimeRemaining());
             currentJob.process(timeToProcessFor, this.time);
             timeLeft -= timeToProcessFor;
             remainingTime -= timeToProcessFor;
 
             if (currentJob.isFinished()) {
-                numJobs ++;
+                numJobs++;
                 totalWaitingTime += currentJob.timeInQueue();
                 queue.poll();
-                size --;
+                size--;
             }
         }
 
@@ -98,6 +101,7 @@ public class Server {
 
     /**
      * Returns the total remaining processing time in the server's queue.
+     *
      * @return The total remaining processing time in the server's queue.
      */
     public double remainingWorkInQueue() {
@@ -106,31 +110,32 @@ public class Server {
 
     /**
      * Returns the number of jobs in the server's queue.
+     *
      * @return The number of jobs currently in the server's queue.
      */
     public int size() {
         return size;
     }
 
-        /**
-         * Renders a visual representation of the server farm.
-         * @param g A Graphics object for drawing.
-         * @param c A color.
-         * @param loc Specifies location on the canvas for drawing.
-         * @param numberOfServers The number of servers in the farm.
-         */
-        public void draw(Graphics g, Color c, double loc, int numberOfServers) {
-            double sep = (ServerFarmViz.HEIGHT - 20) / (numberOfServers + 2.0);
-            g.setColor(Color.BLACK);
-            g.setFont(new Font(g.getFont().getName(), g.getFont().getStyle(), (int) (72.0 * (sep * .5) / Toolkit.getDefaultToolkit().getScreenResolution())));
-            g.drawString("Work: " + (remainingWorkInQueue() < 1000 ? remainingWorkInQueue() : ">1000"), 2, (int) (loc + .2 * sep));
-            g.drawString("Jobs: " + (size() < 1000 ? size() : ">1000"), 5 , (int) (loc + .55 * sep));
-            g.setColor(c);
-            g.fillRect((int) (3 * sep), (int) loc, (int) (.8 * remainingWorkInQueue()), (int) sep);
-            g.drawOval(2 * (int) sep, (int) loc, (int) sep, (int) sep);
-            if (remainingWorkInQueue() == 0) g.setColor(Color.GREEN.darker());
-            else g.setColor(Color.RED.darker());
-            g.fillOval(2 * (int) sep, (int) loc, (int) sep, (int) sep);
-        }
-
+    /**
+     * Renders a visual representation of the server farm.
+     *
+     * @param g               A Graphics object for drawing.
+     * @param c               A color.
+     * @param loc             Specifies location on the canvas for drawing.
+     * @param numberOfServers The number of servers in the farm.
+     */
+    public void draw(Graphics g, Color c, double loc, int numberOfServers) {
+        double sep = (ServerFarmViz.HEIGHT - 20) / (numberOfServers + 2.0);
+        g.setColor(Color.BLACK);
+        g.setFont(new Font(g.getFont().getName(), g.getFont().getStyle(), (int) (72.0 * (sep * .5) / Toolkit.getDefaultToolkit().getScreenResolution())));
+        g.drawString("Work: " + (remainingWorkInQueue() < 1000 ? remainingWorkInQueue() : ">1000"), 2, (int) (loc + .2 * sep));
+        g.drawString("Jobs: " + (size() < 1000 ? size() : ">1000"), 5, (int) (loc + .55 * sep));
+        g.setColor(c);
+        g.fillRect((int) (3 * sep), (int) loc, (int) (.8 * remainingWorkInQueue()), (int) sep);
+        g.drawOval(2 * (int) sep, (int) loc, (int) sep, (int) sep);
+        if (remainingWorkInQueue() == 0) g.setColor(Color.GREEN.darker());
+        else g.setColor(Color.RED.darker());
+        g.fillOval(2 * (int) sep, (int) loc, (int) sep, (int) sep);
+    }
 }
