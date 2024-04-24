b7057470ed84c2beb9d9a612b7e5994cd3c22282
francisohara24 <franciskohara@gmail.com>
2024-04-24 17:11:09 -0400

    Feat: add Stack Interface

---
+++ src/project_7/lab_7/Stack.java
@@ -0,0 +1,39 @@
+/*
+File Name:   Cell.java
+Author:      Francis O'Hara
+Date:        4/2/24
+Description: An interface for implementing a Stack data structure.
+How to Run:  N/A
+*/
+package project_7.lab_7;
+
+public interface Stack<T> {
+
+    /**
+     * Returns the number of items in the stack.
+     * 
+     * @return the number of items in the stack.
+     */
+    public int size();
+
+    /**
+     * Returns the item on the top of the stack.
+     * 
+     * @return the item on the top of the stack.
+     */
+    public T peek();
+
+    /**
+     * Returns and removes the item on the top of the stack.
+     * 
+     * @return the item on the top of the stack.
+     */
+    public T pop();
+
+    /**
+     * Adds the given {@code item} to the top of the stack.
+     * 
+     * @param item the item to be added.
+     */
+    public void push(T item);
+}
