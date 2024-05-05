39392634348d22d6386f39229f52fbf8b544f7bb
francisohara24 <franciskohara@gmail.com>
2024-05-04 20:20:54 -0400

    Feat: add LandscapeDisplay field for visualizing the Sudoku game.

--- src/project_5/Sudoku.java
+++ src/project_5/Sudoku.java
@@ -16,12 +16,18 @@ public class Sudoku {
     private Board board;
 
     /**
+     * The LandscapeDisplay object for visualizing the Sudoku board.
+     */
+    LandscapeDisplay ld;
+
+    /**
      * Creates a new Sudoku board with the given number of randomly placed values on the board.
      *
      * @param numFilled An integer denoting the number of pre-filled values on the board to be created.
      */
     public Sudoku(int numFilled) {
         board = new Board(numFilled);
+        ld = new LandscapeDisplay(board);
     }
 
     /**
@@ -85,10 +91,15 @@ public class Sudoku {
      *
      * @return `true` if the board is solvable. `false` if the board is not solvable.
      */
-    public boolean solve() {
+    public boolean solve(int delay) throws Exception {
         LinkedList<Cell> solution = new LinkedList<>();
         int numUnspecifiedCells = 81 - board.numLocked();
         while (solution.size() < numUnspecifiedCells) {
+            if (delay > 0)
+                Thread.sleep(delay);
+            if (ld != null)
+                ld.repaint();
+
             Cell next = findNextCell();
             while (next == null && !solution.isEmpty()) {
                 Cell current = solution.pop();
@@ -112,17 +123,17 @@ public class Sudoku {
      *
      * @param args An array of Strings denoting the command-line arguments passed to the program.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Sudoku sudoku1 = new Sudoku(0);
         System.out.println("Board 1 Before:\n" + sudoku1.getBoard());
-        if (sudoku1.solve())
+        if (sudoku1.solve(10))
             System.out.println("Board 1 After:\n" + sudoku1.getBoard());
-         else
+        else
             System.out.println("Board 1 cannot be solved!");
 
         Sudoku sudoku2 = new Sudoku(10);
         System.out.println("Board 2 Before:\n" + sudoku2.getBoard());
-        if (sudoku2.solve())
+        if (sudoku2.solve(10))
             System.out.println("Board 2 After:\n" + sudoku2.getBoard());
         else
             System.out.println("Board 2 cannot be solved!");
